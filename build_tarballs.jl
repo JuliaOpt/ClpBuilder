ab3a74a680772c9aed30005fd3782b82c4d9751f
Juan Pablo Vielma <jvielma@mit.edu>
2018-10-01 19:16:36 -0400

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -17,14 +17,30 @@ script = raw"""
 cd $WORKSPACE/srcdir
 cd Clp-releases-1.16.11/
 update_configure_scripts
+# temporary fix
+for path in ${LD_LIBRARY_PATH//:/ }; do
+    for file in $(ls $path/*.la); do
+        echo "$file"
+        baddir=$(sed -n "s|libdir=||p" $file)
+        sed -i~ -e "s|$baddir|'$path'|g" $file
+    done
+done
 mkdir build
 cd build/
-
 if [ $target = "x86_64-w64-mingw32" ] || [ $target = "i686-w64-mingw32" ]; then 
 export LDFLAGS="-L${prefix}/lib -lcoinglpk -lCoinUtils"
 fi
 export CPPFLAGS="-DCOIN_USE_MUMPS_MPI_H"
-../configure --prefix=$prefix --with-pic --disable-pkg-config --host=${target} --enable-shared --enable-static --enable-dependency-linking lt_cv_deplibs_check_method=pass_all --with-asl-lib="-L${prefix}/lib -lasl" --with-asl-incdir="$prefix/include/asl" --with-blas="-L${prefix}/lib -lcoinblas -lgfortran" --with-lapack="-L${prefix}/lib -lcoinlapack" --with-metis-lib="-L${prefix}/lib -lcoinmetis" --with-metis-incdir="$prefix/include/coin/ThirdParty" --with-mumps-lib="-L${prefix}/lib -lcoinmumps" --with-mumps-incdir="$prefix/include/coin/ThirdParty" --with-glpk-lib="-L${prefix}/lib -lcoinglpk" --with-glpk-incdir="$prefix/include/coin/ThirdParty" --with-coinutils-lib="-L${prefix}/lib -lCoinUtils" --with-coinutils-incdir="$prefix/include/coin" --with-osi-lib="-L${prefix}/lib -lOsi" --with-osi-incdir="$prefix/include/coin"
+../configure --prefix=$prefix --with-pic --disable-pkg-config --host=${target} --enable-shared --disable-static \
+--enable-dependency-linking lt_cv_deplibs_check_method=pass_all \
+--with-asl-lib="-L${prefix}/lib -lasl" --with-asl-incdir="$prefix/include/asl" \
+--with-blas="-L${prefix}/lib -lcoinblas" \
+--with-lapack="-L${prefix}/lib -lcoinlapack"\
+--with-metis-lib="-L${prefix}/lib -lcoinmetis" --with-metis-incdir="$prefix/include/coin/ThirdParty"\
+--with-mumps-lib="-L${prefix}/lib -lcoinmumps" --with-mumps-incdir="$prefix/include/coin/ThirdParty" \
+--with-glpk-lib="-L${prefix}/lib -lcoinglpk" --with-glpk-incdir="$prefix/include/coin/ThirdParty" \
+--with-coinutils-lib="-L${prefix}/lib -lCoinUtils" --with-coinutils-incdir="$prefix/include/coin" \
+--with-osi-lib="-L${prefix}/lib -lOsi" --with-osi-incdir="$prefix/include/coin"
 make -j${nproc}
 make install
 
@@ -41,6 +57,7 @@ platforms = [
     Windows(:i686),
     Windows(:x86_64)
 ]
+platforms = expand_gcc_versions(platforms)
 
 # The products that we will ensure are always built
 products(prefix) = [
@@ -51,14 +68,14 @@ products(prefix) = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    "https://github.com/juan-pablo-vielma/OsiBuilder/releases/download/v0.107.9/build_OsiBuilder.v0.107.9.jl",
-    "https://github.com/juan-pablo-vielma/CoinUtilsBuilder/releases/download/v2.10.14/build_CoinUtilsBuilder.v2.10.14.jl",
-    "https://github.com/juan-pablo-vielma/COINGLPKBuilder/releases/download/v1.10.5/build_COINGLPKBuilder.v1.10.5.jl",
-    "https://github.com/juan-pablo-vielma/COINMumpsBuilder/releases/download/v1.6.0/build_COINMumpsBuilder.v1.6.0.jl",
-    "https://github.com/juan-pablo-vielma/COINMetisBuilder/releases/download/v1.3.5/build_COINMetisBuilder.v1.3.5.jl",
-    "https://github.com/juan-pablo-vielma/COINLapackBuilder/releases/download/v1.5.6/build_COINLapackBuilder.v1.5.6.jl",
-    "https://github.com/juan-pablo-vielma/COINBLASBuilder/releases/download/v1.4.6/build_COINBLASBuilder.v1.4.6.jl",
-    "https://github.com/juan-pablo-vielma/ASLBuilder/releases/download/v3.1.0/build_ASLBuilder.v3.1.0.jl"
+    "https://github.com/juan-pablo-vielma/OsiBuilder/releases/download/v0.107.9-1/build_OsiBuilder.v0.107.9.jl",
+    "https://github.com/juan-pablo-vielma/CoinUtilsBuilder/releases/download/v2.10.14-1/build_CoinUtilsBuilder.v2.10.14.jl",
+    "https://github.com/juan-pablo-vielma/COINGLPKBuilder/releases/download/v1.10.5-1/build_COINGLPKBuilder.v1.10.5.jl",
+    "https://github.com/juan-pablo-vielma/COINMumpsBuilder/releases/download/v1.6.0-1/build_COINMumpsBuilder.v1.6.0.jl",
+    "https://github.com/juan-pablo-vielma/COINMetisBuilder/releases/download/v1.3.5-1/build_COINMetisBuilder.v1.3.5.jl",
+    "https://github.com/juan-pablo-vielma/COINLapackBuilder/releases/download/v1.5.6-1/build_COINLapackBuilder.v1.5.6.jl",
+    "https://github.com/juan-pablo-vielma/COINBLASBuilder/releases/download/v1.4.6-1/build_COINBLASBuilder.v1.4.6.jl",
+    "https://github.com/juan-pablo-vielma/ASLBuilder/releases/download/v3.1.0-1/build_ASLBuilder.v3.1.0.jl"
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
